689492fb31f835d0775e1b3e21c1c3c709484155
patrick mugambi <patrick@kali.patrick.com>
2022-12-09 13:49:20 +0300

    detail view post

---
+++ src/components/AuthorCard.vue
@@ -0,0 +1,17 @@
+<script>
+import { CreateURL } from '../utils'
+
+export default {
+	props: {
+		author: {
+			type: Object,
+			required: true
+		}
+	},
+	setup () {
+		return {
+			CreateURL
+		}
+	}
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import store from '../store'
 
 const routes = [
   {
@@ -14,12 +15,36 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/authors',
+    name: 'Authors',
+    component: () => import('../views/AllAuthors.vue'),
+    },
+    {
+    path: '/post/:id',
+    name: 'Post',
+    component: () => import('../views/post/_id.vue'),
+    },
+    {
+    path: '/author/:id',
+    name: 'Author',
+    component: () => import('../views/author/_id.vue'),
+    }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+router.afterEach((to, from) => {
+	// Only run if you are coming from a route
+	if (from.name) { 
+		document.documentElement.scrollTop = 0
+		
+		// Close menu after navigation
+		store.dispatch('CloseMenu')
+	}
 })
 
 export default router
